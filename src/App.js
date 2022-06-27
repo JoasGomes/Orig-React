5b748f5f95da31518df8eddd6617a15efbf29eab
JoasGomes <jojoca.vitor777@gmail.com>
2022-06-27 12:14:17 -0300

    webpack e babel

---
+++ src/App.js
@@ -0,0 +1,6 @@
+import React from 'react';
+
+const App = () => {
+  return <a href="https://www.origamid.com">Origamid</a>;
+};
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  devServer: {
+    static: './',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-react'],
+          },
+        },
+      },
+    ],
+  },
+};
